0de9d59e50d467e77adfd5554c16288d38d52646
Seth Axen <seth.axen@gmail.com>
2016-09-15 23:58:49 -0700

    updated version to fix pypi issues

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='sdaxen_python_utilities',
     packages=['python_utilities'],
-    version='0.1.1',
+    version='0.1.2',
     description='A collection of useful tools for common Python tasks',
     author='Seth Axen',
     author_email='seth.axen@gmail.com',
     url='https://github.com/sdaxen/python_utilities',
-    download_url='https://github.com/sdaxen/python_utilities/tarball/0.1.1',
+    download_url='https://github.com/sdaxen/python_utilities/tarball/0.1.2',
     keywords=['scripting', 'logging', 'io', 'plotting'],
     classifiers=[],
 )
