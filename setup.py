616206bf3644df9a9094cf4498e73e6128ea3fd7
Seth Axen <seth.axen@gmail.com>
2016-09-15 23:27:51 -0700

    updated version

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from distutils.core import setup
 
 setup(
-    name='python_utilities',
+    name='sdaxen_python_utilities',
     packages=['python_utilities'],
-    version='0.1.0',
+    version='0.1.1',
     description='A collection of useful tools for common Python tasks',
     author='Seth Axen',
     author_email='seth.axen@gmail.com',
     url='https://github.com/sdaxen/python_utilities',
-    download_url='https://github.com/sdaxen/python_utilities/tarball/0.1.0',
+    download_url='https://github.com/sdaxen/python_utilities/tarball/0.1.1',
     keywords=['scripting', 'logging', 'io', 'plotting'],
     classifiers=[],
 )
+
