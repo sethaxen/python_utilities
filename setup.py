79a2ea4cc73faf66ceedec64ea21585e5189bf54
Seth Axen <seth.axen@gmail.com>
2016-09-15 21:28:51 -0700

    added files for pypi registration

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='python_utilities',
+    packages=['python_utilities'],
+    version='0.1.0',
+    description='A collection of useful tools for common Python tasks',
+    author='Seth Axen',
+    author_email='seth.axen@gmail.com',
+    url='https://github.com/sdaxen/python_utilities',
+    download_url='https://github.com/sdaxen/python_utilities/tarball/0.1.0',
+    keywords=['scripting', 'logging', 'io', 'plotting'],
+    classifiers=[],
+)
