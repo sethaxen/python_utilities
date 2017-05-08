353352e97febd31b7d4c49c2d983c2ff79cfee17
Seth Axen <seth.axen@gmail.com>
2017-05-08 00:18:12 -0700

    Add classifiers

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
 from distutils.core import setup
 
+classifiers = ['Programming Language :: Python',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3.6',
+               'License :: OSI Approved :: MIT License',
+               'Operating System :: OS Independent',
+               'Development Status :: 4 - Beta',
+               'Intended Audience :: Developers',
+               'Intended Audience :: System Administrators',
+               'Intended Audience :: Science/Research',
+               'Intended Audience :: Education',
+               'Topic :: Scientific/Engineering',
+               'Topic :: Software Development :: Libraries :: Python Modules',
+               'Topic :: System :: Distributed Computing',
+               'Topic :: System :: Logging',
+               'Topic :: Utilities'
+               ]
+
 setup(
     name='sdaxen_python_utilities',
     packages=['python_utilities', 'python_utilities.plotting'],
@@ -10,6 +27,6 @@ setup(
     url='https://github.com/sdaxen/python_utilities',
     download_url='https://github.com/sdaxen/python_utilities/tarball/0.1.3',
     keywords=['scripting', 'logging', 'io', 'plotting'],
-    classifiers=[],
+    classifiers=classifiers
 )
 
